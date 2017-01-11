d4b35b9fca3eaf370c5d7bb24f646cc2e0be2568
asrosenberg <rosenberg.1108@osu.edu>
2017-01-11 13:36:36 -0500

    initial commit

---
+++ R/load_data.R
@@ -0,0 +1,14 @@
+# ' Load dataset from inst/extdata
+#'
+#' Loads a dataset from inst/extdata into memory.
+#' @param dataset the name of the dataset to load
+#' @return data set
+#' @import data.table
+#' @export
+load_dataset <- function(dataset)
+{
+  file_name <- system.file("extdata", paste0(dataset,".csv"),
+    package = "causalA16")
+  DATA <- read.csv(file_name)
+}
+
