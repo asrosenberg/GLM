f67f477739c83e43691701e6e5fc6d366d158f43
Andrew Rosenberg <rosenberg.1108@ps-stats05.asc.ohio-state.edu>
2017-02-14 10:35:33 -0500

    recitation 6 script

---
+++ recitation/recitation6.R
@@ -0,0 +1,15 @@
+n <- 500
+x1 <- rnorm(n)
+x2 <- rnorm(n)
+x3 <- rnorm(n)
+b0 <- 2
+b1 <- -2
+b2 <- 1
+b3 <- -5
+y <- b0 + b1 * x1 + b2 * x2 + b3 * x3 + rnorm(n)
+y <- b0 * b1 * x1 * b2 * x2 * b3 * x3 + rnorm(n)
+plot(x1, y)
+plot(x2, y)
+plot(x3, y)
+plot(fitted(lm(y ~ x1 + x2 + x3)), resid(lm(y ~ x1 + x2 + x3)))
+plot(lm(y ~ x1 + x2 + x3))
